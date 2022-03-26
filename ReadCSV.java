eeec986b63d1f9a19b70cea99047457ffafe36e7
DexterHK <84778260+DexterHK@users.noreply.github.com>
2022-03-26 20:37:13 +0100

    Add files via upload

---
+++ ReadCSV.java
@@ -0,0 +1,26 @@
+package StudentenVerwaltung;
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+import java.nio.charset.Charset;
+
+public class ReadCSV {
+	private	ArrayList<String> csvInput = new ArrayList<String>();
+public void readFile(String fileLocation) throws FileNotFoundException
+{
+	Database databaseObj = new Database();
+	File getCSVFile = new File(fileLocation);
+	Scanner input = new Scanner(getCSVFile);
+	
+	while(input.hasNext())
+	{
+		 csvInput.add("create "+ input.next());
+	}
+	 
+		databaseObj.dex(csvInput);
+
+	input.close();
+	
+}
+	
+}
---
+++ Student.java
@@ -0,0 +1,62 @@
+package StudentenVerwaltung;
+
+public class Student {
+	// UI -> API -> Gesch�ftslogic(DOMAIN) -> Infrastruktur(ORM persistence)
+	//Attributes
+public String name;
+private String vorname;
+private int matknr;
+private int semesterzahl;
+public static boolean quit = false;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getVorname() {
+	return vorname;
+}
+public void setVorname(String vorname) {
+	this.vorname = vorname;
+}
+public int getMatknr() {
+	return matknr;
+}
+public void setMatknr(int matknr) {
+	this.matknr = matknr;
+}
+public int getSemesterzahl() {
+	return semesterzahl;
+}
+public void setSemesterzahl(int semesterzahl) {
+	this.semesterzahl = semesterzahl;
+}
+//Private counter;
+//Singleton
+//Constructors
+
+Student(String name, String vorname, int matknr,int semesterzahl)
+{
+	
+this.name = name;
+this.vorname = vorname;
+this.matknr = matknr;
+this.semesterzahl = semesterzahl;
+	
+	
+}
+Student(String name, String vorname, int matknr)
+{
+this(name,vorname,matknr,1);
+	
+}
+   
+    
+	
+	
+
+
+//Methodes
+
+}
