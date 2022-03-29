cf3d5cd15d66482af02c1d57d9fe64d3e089f5e4
DexterHK <84778260+DexterHK@users.noreply.github.com>
2022-03-29 15:41:41 +0200

    Add files via upload

---
+++ StudentenVerwaltung/Main.java
@@ -0,0 +1,19 @@
+package StudentenVerwaltung;
+
+import java.io.IOException;
+import java.util.*;
+
+import StudentenVerwaltung.UI.Controller;
+import StudentenVerwaltung.UI.Database;
+
+
+
+
+public class Main {
+	public static void main(String[] arg) throws IOException {
+
+Controller cr = new Controller(); 
+		cr.controller();
+	
+	}
+}
